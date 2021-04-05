999acff6f841e8e403b0005b7a8b393103f9fc46
Jake Hayes <thejayhaykid@users.noreply.github.com>
2021-04-05 15:53:37 -0400

    Updated Seed, should work now

--- api/db/seed.js
+++ api/db/seed.js
@@ -1,46 +1,63 @@
 /* eslint-disable no-console */
-const { PrismaClient } = require('@prisma/client')
-const dotenv = require('dotenv')
+const { PrismaClient } = require('@prisma/client');
+const dotenv = require('dotenv');
+const data = require('./seed-data.json');
 
-dotenv.config()
-const db = new PrismaClient()
+dotenv.config();
+const db = new PrismaClient();
 
 async function main() {
-  // https://www.prisma.io/docs/guides/prisma-guides/seed-database
-  // Seed data is database data that needs to exist for your app to run.
-  // Ideally this file should be idempotent: running it multiple times
-  // will result in the same database state (usually by checking for the
-  // existence of a record before trying to create it). For example:
-  /*
-    const result = await db.user.createMany({
-      data: [
-        { email: "alice@example.com" },
-        { email: "mark@example.com" },
-        { email: "jackie@example.com" },
-        { email: "bob@example.com" },
-      ],
-      skipDuplicates: true, // Supported with Postgres database
-    })
-    console.log(`Created ${result.count} users!`)
-  */
-  // Note: createMany creates multiple records in a transaction.
-  // To enable this feature, add createMany to previewFeatures in your schema.
-  // See: https://www.prisma.io/docs/reference/api-reference/prisma-client-reference#createmany-preview
-  // Note: createMany is not supported by SQLite.
-  //
-  // Example without createMany (supported by all databases):
-  /*
-    const existing = await db.user.findMany({ where: { email: 'admin@email.com' }})
-    if (!existing.length) {
-      await db.user.create({ data: { name: 'Admin', email: 'admin@email.com' }})
-    }
-  */
+  const exWorkouts = await db.workout.findMany();
+  const exExercises = await db.exercise.findMany();
+  const exUsers = await db.user.findMany();
 
-  console.info('No data to seed. See api/db/seed.js for info.')
+  const newWorkouts = data.workouts;
+  const newExercises = data.exercises;
+  const newUsers = data.users;
+
+  newWorkouts.forEach(async (workout) => {
+    if (!exWorkouts.find((ex) => ex.title === workout.title)) {
+      const result = await db.workout.create({ data: workout });
+      if (result.error) {
+        throw new Error(
+          `Error creating record!\nError: ${
+            result.error
+          }\nRecord: ${JSON.stringify(workout)}`,
+        );
+      }
+    }
+  });
+
+  newExercises.forEach(async (exercise) => {
+    if (!exExercises.find((ex) => ex.title === exercise.title)) {
+      const result = await db.exercise.create({ data: exercise });
+      if (result.error) {
+        throw new Error(
+          `Error creating record!\nError: ${
+            result.error
+          }\nRecord: ${JSON.stringify(exercise)}`,
+        );
+      }
+    }
+  });
+
+  newUsers.forEach(async (user) => {
+    if (!exUsers.find((ex) => ex.title === user.title)) {
+      const result = await db.user.create({ data: user });
+      if (result.error) {
+        throw new Error(
+          `Error creating record!\nError: ${
+            result.error
+          }\nRecord: ${JSON.stringify(user)}`,
+        );
+      }
+    }
+  });
 }
 
 main()
   .catch((e) => console.error(e))
   .finally(async () => {
-    await db.$disconnect()
-  })
+    console.log(`Completed seed, disconnecting db`);
+    await db.$disconnect();
+  });
